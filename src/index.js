c1382ecd10d5266e62ce3a001017fcbc271f5daa
xGlitchWolfx <uspwolf2563@gmail.com>
2025-06-01 13:42:05 -0500

    correccion de errores 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
